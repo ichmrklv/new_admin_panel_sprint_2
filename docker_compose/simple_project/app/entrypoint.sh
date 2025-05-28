a148bdee3ff43f958ff00c3e49d44b2b8fdda3f3
ichmrklv <merrkl@mail.ru>
2025-05-28 15:18:38 +0300

    Wait for DB before migrations using netcat

--- docker_compose/simple_project/app/entrypoint.sh
+++ docker_compose/simple_project/app/entrypoint.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 
+# цикл ожидания запуска БД
+echo "Waiting for database to be ready at $DB_HOST:$DB_PORT..."
+while ! nc -z $DB_HOST $DB_PORT; do
+  echo "Waiting for $DB_HOST:$DB_PORT..."
+  sleep 0.1
+done
+
+echo "Database is up!"
+
 echo "Applying database migrations..."
 python manage.py migrate --noinput
 
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
-exec "$@"
+exec "$@" # передача gunicorn
